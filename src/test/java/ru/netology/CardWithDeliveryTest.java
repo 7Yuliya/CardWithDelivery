8b05afa3ae61c283268952b4295ef7e1ba7f3794
Юлия Алексина <uliyaalexina@mail.ru>
2022-09-12 14:16:05 -0700

    Fixed src/

--- src/test/java/ru/netology/CardWithDeliveryTest.java
+++ src/test/java/ru/netology/CardWithDeliveryTest.java
@@ -7,11 +7,20 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 import java.lang.module.Configuration;
 import java.time.Duration;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.$x;
@@ -20,20 +29,20 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CardWithDeliveryTest {
 
+
     @Test
     public void test1() {
 
-        open(" http://localhost:9999");
-        $x("//* [@placeholder = \"Город\"]").setValue("Казань");
-        $x("//*[@placeholder = \"date\"]").setValue("15.09.2022");
+        open("http://localhost:9999");
+        $x("//*[@placeholder = \"Город\"]").setValue("Казань");
+        String verificationDate = LocalDate.now().plusDays(3).format(DateTimeFormatter.ofPattern("20.09.2022"));
+        $x("//*[@data-test-id = \"date\"]//self::input").doubleClick().sendKeys(Keys.DELETE + verificationDate);
         $x("//*[@name = \"name\"]").setValue("Иванов Андрей");
-        $x("//* [@name = \"phone\"]").setValue("+79214567845");
-        $x("//* [@data-test-id = \"agreement\"]").click();
-        $x("//* [@class =\"button__text\" ]").click();
-        // String text = $x("//* [contains(text(),'Успешно!Встреча успешно забронирована на 15.09.2022')]").getText();
-        //assertEquals("Успешно!Встреча успешно забронирована на 15.09.2022", text);
-        $x("//* [contains(text(),\"Успешно!Встреча успешно забронирована на 15.09.2022\")]").should(visible, Duration.ofSeconds(10));
+        $x("//*[@name = \"phone\"]").setValue("+79214567845");
+        $x("//*[@data-test-id = \"agreement\"]").click();
+        $x("//*[@class =\"button__text\" ]").click();
+        $x("//* [contains(text(),\"Успешно!\")]").should(visible, Duration.ofSeconds(12));
     }
-    }
+}
 
 
